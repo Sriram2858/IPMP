361943febd39aeb6fd749da75a9d7531e2526a05
Sriram2858 <144020808+Sriram2858@users.noreply.github.com>
2024-12-26 23:49:42 +0530

    Create SpiralMatrix.cpp

---
+++ week1/array/medium/SpiralMatrix.cpp
@@ -0,0 +1,25 @@
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int top = 0, bottom = m - 1, left = 0, right = n - 1;
+    vector<int> r;
+    while(top <= bottom && left <= right){
+        for(int i = left; i <= right; i++)
+            r.push_back(matrix[top][i]);
+        top++;
+        for(int i = top; i <= bottom; i++)
+            r.push_back(matrix[i][right]);
+        right--;
+        if(top <= bottom){
+            for(int i = right; i >= left; i--)
+                r.push_back(matrix[bottom][i]);
+            bottom--;
+        }
+        if(left <= right){
+            for(int i = bottom; i >= top; i--)
+                r.push_back(matrix[i][left]);
+            left++;
+        }
+    }
+    return r;
+}
